a22714cac491cf00ae618102f15a7bf65fbec934
itsdancedrick <42290274+itsdancedrick@users.noreply.github.com>
2019-04-27 14:24:17 +0800

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  users: object;
+
+  h1Style: boolean = false;
+
+  constructor(private data: DataService) {
+
+   }
+
+  
+
+  ngOnInit() {
+    this.data.getUsers().subscribe( data => {
+      this.users = data 
+      
+    })
+  }
+
+ 
+}
